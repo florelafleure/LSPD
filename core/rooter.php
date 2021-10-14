0866f8a9b7935a4c7873be2ed95f3330b16a53cc
Flore La Fleure <89973963+florelafleure@users.noreply.github.com>
2021-10-14 15:19:31 +0200

    maj rooter

--- core/rooter.php
+++ core/rooter.php
@@ -2,15 +2,15 @@
     function router(){
         $pageName = str_replace('/', '', isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : '');
         switch($pageName){
-            case 'formulaire_1':
-            case 'cible_1':
-                return "cible/cible1.php";
-            case 'cible_2':
-                return "cible/cible2.php";
-            case 'cible_3':
-                return "cible/cible3.php";
-            case 'cible_tp_1';
-                return 'cible/cibletp1.php';
+            case '':
+            case '':
+                return "";
+            case '':
+                return "";
+            case '':
+                return "";
+            case '';
+                return '';
             
         }
     }
